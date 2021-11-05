37b4722773955ab55d90d8b75425c6740271f9dc
akshith6212 <akshith6212@gmail.com>
2021-11-05 08:47:18 +0530

    sample check added

--- code and results/Score.py
+++ code and results/Score.py
@@ -10,65 +10,70 @@ class GetScore:
     result1 = Unpack(filename2)
     arr1 = result1.getarray()
 
-    n0 = len(arr0)
-    n1 = len(arr1)
+    self.n0 = len(arr0)
+    self.n1 = len(arr1)
 
-    print(n0,n1)
+    print(self.n0,self.n1)
 
-    n = max(n0,n1)/min(n0,n1)
+    n = max(self.n0,self.n1)/min(self.n0,self.n1)
 
     indices = []
 
-    for i in range(min(n0,n1)):
+    for i in range(min(self.n0,self.n1)):
       indices.append(int(i*n))
 
     # store the sampled values in arr
     arr = []
-    if(n0 > n1):
-      for i in range(min(n0,n1)):
+    if(self.n0 > self.n1):
+      for i in range(min(self.n0,self.n1)):
         arr.append(arr0[indices[i]])
       # print(len(arr))
 
       self.diff = 0
-      for i in range(min(n0,n1)):
+      for i in range(min(self.n0,self.n1)):
         self.diff += abs(arr[i]-arr1[i])
       print("Absolute difference  :", int(self.diff))
 
-      sum0 = 0
-      sum1 = 0
+      self.sum0 = 0
+      self.sum1 = 0
       for i in arr:
-        sum0 += i
+        self.sum0 += i
       for i in arr1:
-        sum1 += i
+        self.sum1 += i
 
-      avg = (sum0+sum1)/2
+      avg = (self.sum0+self.sum1)/2
 
       self.percentage_similarity = ((avg - self.diff)/avg)*100
 
       print("Percentage similarity:", round(self.percentage_similarity, 2),"%")
     
     else:
-      for i in range(min(n0,n1)):
+      for i in range(min(self.n0,self.n1)):
         arr.append(arr1[indices[i]])
       # print(len(arr))
 
       self.diff = 0
-      for i in range(min(n0,n1)):
+      for i in range(min(self.n0,self.n1)):
         self.diff += abs(arr[i]-arr0[i])
       print("Absolute difference  :", int(self.diff))
 
-      sum0 = 0
-      sum1 = 0
+      self.sum0 = 0
+      self.sum1 = 0
       for i in arr:
-        sum0 += i
+        self.sum0 += i
       for i in arr0:
-        sum1 += i
+        self.sum1 += i
 
-      avg = (sum0+sum1)/2
+      avg = (self.sum0+self.sum1)/2
 
       self.percentage_similarity = ((avg - self.diff)/avg)*100
 
       print("Percentage similarity:", round(self.percentage_similarity, 2),"%")
 
+  # Returns the absolute difference and percentage similarity
   def GetData(self):
     return (int(self.diff), round(self.percentage_similarity, 2))
+
+  # Returns Debug Data if required
+  def GetDebugData(self):
+    return (self.n0, self.n1, self.sum0, self.sum1)
--- code and results/main.py
+++ code and results/main.py
@@ -1,7 +1,25 @@
 from Score import GetScore
 
-score0 = GetScore('capture_levitating-0.pcap','capture_levitating-1.pcap')
+checkfilename = 'capture_levitating-2.pcap'
 
+score0 = GetScore('capture_levitating-0.pcap',checkfilename)
+score1 = GetScore('capture_levitating-1.pcap',checkfilename)
 
+score2 = GetScore('capture_hui-0.pcap',checkfilename)
+score3 = GetScore('capture_hui-1.pcap',checkfilename)
 
-print(score0.GetData())
+# For Debug data
+# DebugData0 = score0.GetDebugData()
+
+score0 = score0.GetData()[1]
+score1 = score1.GetData()[1]
+score2 = score2.GetData()[1]
+score3 = score3.GetData()[1]
+
+score_levitating = (score0 + score1)/2
+score_hui = (score2 + score3)/2
+
+if(score_levitating > score_hui):
+  print("Closer to levitating")
+else:
+  print("Closer to hui")
